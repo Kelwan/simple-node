8e03a184c6059db2b5e7ea11e1bcf9ee1f84a3dc
Kelwan <austin@kelway.ca>
2017-05-17 09:51:10 -0400

    Changes problem.txt to uppercase and one coherent piece

---
+++ simple-node.js
@@ -0,0 +1,18 @@
+var through = require('through2');
+var fs = require('fs');
+var split = require('split');
+
+linecount = 0;
+
+var through = through(function(buffer, _, next){
+  buffer = buffer.toString().toUpperCase();
+  this.push(buffer + ' ');
+  next();
+});
+
+
+fs.createReadStream('problem.txt')
+  .pipe(split())
+  .pipe(through)
+  .pipe(process.stdout)
+;
